666a425ec8be8c9c5f35e4934c351ae6e3c9f512
Tom McKenzie <tmckenzie.nz@gmail.com>
2025-04-11 15:24:22 +1000

    basic eleventy structure established

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+    // Copy the "images" and "css" directory to the output
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("css");
+    
+    return {
+        dir: {
+            input: ".",
+            includes: "_includes",
+            output: "_site"
+        },
+        templateFormats: ["njk", "md", "html"],
+        htmlTemplateEngine: "njk"
+    };
+}; 
